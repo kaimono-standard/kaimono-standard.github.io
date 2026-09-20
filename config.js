7626d94adaa4fc820838373211cb6cefee48f9bf
KojimaHyui01 <kojima.hyui@gmail.com>
2026-09-20 17:18:04 +0900

    Add researched electric kettle comparison guide

--- config.js
+++ config.js
@@ -9,6 +9,11 @@ window.SITE_CONFIG = {
     glassFilm: "https://a.r10.to/h5IJUB",
     necklace: "https://a.r10.to/hPz3ch",
     phonePouch: "https://a.r10.to/hgF2SQ",
-    chopsticks: "https://a.r10.to/h5J5XZ"
+    chopsticks: "https://a.r10.to/h5J5XZ",
+    kettleTfal: "https://hb.afl.rakuten.co.jp/ichiba/57b73b8e.0a6efe33.57b73b8f.82ca1d5b/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fbellevie-harima%2Ft-fal-350%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIyNDB4MjQwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D",
+    kettleZojirushi: "https://hb.afl.rakuten.co.jp/ichiba/57b73c89.a9ad4f57.57b73c8a.901a6902/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fr-kojima%2F4974305232069%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIyNDB4MjQwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D",
+    kettleTiger: "https://hb.afl.rakuten.co.jp/ichiba/57b73c8d.5c4b1523.57b73c8e.0a8c127f/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Ftiger-official-store%2Fpcv-a0%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIyNDB4MjQwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D",
+    kettleBalmuda: "https://hb.afl.rakuten.co.jp/ichiba/57b73c97.9136d5e4.57b73c98.90aa48c5/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fimportshopaqua%2Faq000199%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIyNDB4MjQwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D",
+    kettleIris: "https://hb.afl.rakuten.co.jp/ichiba/57b73cab.fc9d3f8d.57b73cac.d7bbcabb/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Firisplaza-r%2F202601%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIyNDB4MjQwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D"
   }
 };
